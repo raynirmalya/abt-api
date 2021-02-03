e6184e9dfbc1368c977ea77d8578f5fce9060c77
NirmalyaHCL <nirmalya.roy.21@gmail.com>
2021-02-03 18:30:07 +0530

    db connectivity change
    Signed-off-by: Nirmalya

--- dbconfig.py
+++ dbconfig.py
@@ -2,9 +2,9 @@
 
 ##mysql = MySQL()
 
-server = 'LAPTOP-52PJGRUU\SQLEXPRESS' ##'azure-jbatty.database.windows.net'
+server = 'quations.database.windows.net' ##'azure-jbatty.database.windows.net'
 database = 'questionnairedb'
-username = 'admin'
+username = 'qadmin'
 password = 'Debugger1$'
 port = 1433
 driver = '{ODBC Driver 17 for SQL Server}'
